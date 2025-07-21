1562403cb807466732288ae8727479c329dd7b16
Rechcel Toledo <rechceltoledo@gmail.com>
2025-07-22 05:57:19 +0800

    changed: backend url

--- haughton_clone/base_url.js
+++ haughton_clone/base_url.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001';
-//const BASE_URL = 'https://haughton-ventures-production.up.railway.app';
+//const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://haughton-ventures-production.up.railway.app';
 
 export default BASE_URL;
